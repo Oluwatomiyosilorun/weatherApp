0b04c850d29be7014b7bc833216640cdbf20b2d2
toyosi <toyosiazuh.at@gmail.com>
2022-05-19 13:07:30 +0100

    initial, created home and search components + scss

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <Search />
+  </div>
+</template>
+
+<script>
+import Search from "./Search.vue";
+export default {
+  components: {
+    Search,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  background-image: url("../assets/night.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size:cover;
+  p {
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="search">
+    <aside>
+      <input type="search" placeholder="Another Location" name="location" />
+      <span>&#x1F50D;</span>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.search {
+  position: absolute;
+  width: 400px;
+  height: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0;
+  background-color: #0d232f;
+  border: 1px solid #0d232f;
+  opacity: 0.9;
+  aside {
+    padding: 10%;
+    display: flex;
+    input {
+      width: 80%;
+      height: 35px;
+      border: 0;
+      background-color: inherit;
+      border-bottom: 1px solid whitesmoke;
+      box-shadow: none;
+      outline: none;
+      color: whitesmoke;
+    }
+    span {
+      background-color: #d66c06;
+      padding: 7%;
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
+}
+</style>
